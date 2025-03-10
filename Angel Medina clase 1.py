e13b62445b5966198b542155a112a191f625e831
admc33 <angeldmc0078@gmail.com>
2025-03-10 11:13:32 -0400

    Add files via upload

---
+++ Angel Medina clase 1.py
@@ -0,0 +1,9 @@
+nombre = "Angel"
+edad = 18
+ciudad = "Maracaibo"
+print (f"Hola mi nombre es {nombre}, tengo {edad} años y vivo en {ciudad}")  
+materia1 = 16
+materia2 = 20
+materia3 = 18
+promedio = (materia1 + materia2 + materia3) // 3
+print (f"El promedio de {nombre} es {promedio}")
