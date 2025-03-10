110955a6f83c0ada115bc7ec1be2d668c782b003
admc33 <angeldmc0078@gmail.com>
2025-03-10 12:02:32 -0400

    Add files via upload

---
+++ Angel Medina.py
@@ -0,0 +1,10 @@
+num1 = input("insertar digito:")
+num2 = input("insertar digito:")
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+print (suma)
+print (resta)
+print (multiplicacion)
+print (division)
